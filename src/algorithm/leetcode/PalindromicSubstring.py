fbe6b8e45b830948d72468e068b5ba469d7d90fd
SJParkkk <SJParkk@github.com>
2022-06-13 00:26:58 +0900

    중간순회 개발중

--- src/algorithm/leetcode/PalindromicSubstring.py
+++ src/algorithm/leetcode/PalindromicSubstring.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 class Solution:
 
     @staticmethod
@@ -11,13 +14,44 @@ class Solution:
         return True
 
     def longest_palindrome(self, s: str) -> str:
-        answer = '' if len(s) != 1 else s
-
-        for count in range(1, len(s) + 1):
-            if len(s) > 2 and count == 1:
-                continue
+        answer = ''
+        palindromes = deque()
+        numbers = [i for i in range(len(s) + 1)]
+        numbers_count = len(numbers) if len(numbers) % 2 == 0 else len(numbers) -1
+        middle_index = int(numbers_count / 2)
+        left, right = numbers[:middle_index], numbers[middle_index:]
+        for count in right:
             for i in range(0, len(s)):
                 new_s = s[i: i+count]
                 if self.is_palindromic(new_s) and len(new_s) == count:
-                    return new_s
-        return answer
+                    palindromes.append(new_s)
+        if palindromes:
+            answer = palindromes.pop()
+            return answer
+                # palindromes = deque()
+        for count in reversed(right):
+            for i in range(0, len(s)):
+                new_s = s[i: i + count]
+                if self.is_palindromic(new_s) and len(new_s) == count:
+                    palindromes.append(new_s)
+        if palindromes:
+            return palindromes.pop()
+
+        # 중간값 구하고 젤 큰 값 pop 없으면
+
+        # for count in range(len(s) + 1, 0, -1):
+        #     for i in range(0, len(s)):
+        #         new_s = s[i: i+count]
+        #         if self.is_palindromic(new_s) and len(new_s) == count:
+        #             palindromes.append(new_s)
+        #     if palindromes:
+        #         return palindromes.popleft()
+        # if not longest_count:
+        #     longest_count = len(new_s)
+        # if len(new_s) > longest_count:
+        #     longest_count = len(new_s)
+        # longest_count = max([len(e) for e in answers])
+        # for palindrome in palindromes:
+        #     if len(palindrome) == longest_count:
+        #         answer = palindrome
+        #         break
--- tests/test_PalindromicSubstring.py
+++ tests/test_PalindromicSubstring.py
@@ -15,22 +15,30 @@ def test_is_palindromic():
 def test_longest_palindrome():
     solution = Solution()
 
-    s = "babad"
-    assert "bab" == solution.longest_palindrome(s)
-
-    s = "cbbd"
-    output = "bb"
-    assert output == solution.longest_palindrome(s)
-
-    s = "a"
-    output = "a"
-    assert output == solution.longest_palindrome(s)
-
+    # # s = "babad"
+    # # assert "bab" == solution.longest_palindrome(s)
+    #
+    # s = "cbbd"
+    # output = "bb"
+    # assert output == solution.longest_palindrome(s)
+    #
+    # s = "a"
+    # output = "a"
+    # assert output == solution.longest_palindrome(s)
+    #
     s = "ac"
     output = "a"
     assert output == solution.longest_palindrome(s)
 
-    s = "bb"
-    output = "bb"
-    assert output == solution.longest_palindrome(s)
+    # s = "bb"
+    # output = "bb"
+    # assert output == solution.longest_palindrome(s)
+
+    # s = "klvxwqyzugrdoaccdafdfrvxiowkcuedfhoixzipxrkzbvpusslsgfjocvidnpsnkqdfnnzzawzsslwnvvjyoignsfbxkgrokzyusxikxumrxlzzrnbtrixxfioormoyyejashrowjqqzifacecvoruwkuessttlexvdptuvodoavsjaepvrfvbdhumtuvxufzzyowiswokioyjtzzmevttheeyjqcldllxvjraeyflthntsmipaoyjixygbtbvbnnrmlwwkeikhnnmlfspjgmcxwbjyhomfjdcnogqjviggklplpznfwjydkxzjkoskvqvnxfzdrsmooyciwulvtlmvnjbbmffureoilszlonibbcwfsjzguxqrjwypwrskhrttvnqoqisdfuifqnabzbvyzgbxfvmcomneykfmycevnrcsyqclamfxskmsxreptpxqxqidvjbuduktnwwoztvkuebfdigmjqfuolqzvjincchlmbrxpqgguwuyhrdtwqkdlqidlxzqktgzktihvlwsbysjeykiwokyqaskjjngovbagspyspeghutyoeahhgynzsyaszlirmlekpboywqdliumihwnsnwjc"
+    # 변태 아니냐 진짜로?
+
+    # assert output == solution.longest_palindrome(s)
+
+
+
 
